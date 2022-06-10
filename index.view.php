4e08059313a3c57bb5392ebdd189eb8b6262db3c
EilynAntirrhinum <elyrhinum@gmail.com>
2022-06-10 14:17:45 +0300

    editing main page

--- index.view.php
+++ index.view.php
@@ -1,50 +1,74 @@
 <?php include_once $_SERVER["DOCUMENT_ROOT"] . "/views/components/header.php"; ?>
 
 <main>
+<!--    <div class="container">-->
+<!--        <div class="row">-->
+<!--            <div class="col">-->
+<!--                <img src="/public/img/icons/icon-person.svg" alt="">-->
+<!--            </div>-->
+<!---->
+<!--            <div class="col posDialogWindow">-->
+<!--                <canvas id="dialogWindow"></canvas>-->
+<!--            </div>-->
+<!--            <p class="fs-5 posTextTrovald">Приветствую, меня зовут Торвальд, я буду твоим спутником на всем твоем сложном пути познания. <br><br>-->
+<!---->
+<!--                Чтобы я смог делиться с тобой своими знаниями тебе необходимо авторизироваться!</p>-->
+<!--            <div class="col posAuth">-->
+<!---->
+<!--                <form class="auth" action="">-->
+<!--                    <div class="input-group mb-4 p-3 sizeBlockAuth">-->
+<!--                        <div class="col">-->
+<!--                            <h3 class="mb-3">АВТОРИЗАЦИЯ</h3>-->
+<!--                            <div>-->
+<!--                                <input class=" col-sm-2 form-control mb-4 borderNone" type="text" placeholder="Логин">-->
+<!--                                <input class=" col-sm-2 form-control mb-4 borderNone" type="password" placeholder="Пароль">-->
+<!--                            </div>-->
+<!--                            <div>-->
+<!--                                <button class="btn btn-light  mt-4 borderNone">ВОЙТИ</button>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                </form>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
 
+    <section class="about-python">
+        <p class="header-python">Python</p>
+        <p class="text-python">Различные языки программирования обычно доминируют в какой-то отрасли (или нескольких), для работы в которой они хорошо подходят. Но это не значит, что программист ограничен использовать строго определённый инструмент, поэтому любой язык общего назначения, такой как Python, может применять для создания чего-угодно.</p>
+    </section>
 </main>
 
-<?php include_once $_SERVER["DOCUMENT_ROOT"] . "/views/components/footer.php"; ?>
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <img src="/public/img/icons/icon-person.svg" alt="">
+    <div id="header-courses">
+        <h3>Курсы</h3>
+    </div>
+
+<main>
+    <section id="courses">
+        <div class="course">
+            <div class="edit-btn-course">
+                <button>
+                    <img src="/public/img/icons/elipsis.svg" alt="Редактировать">
+                </button>
             </div>
 
-            <div class="col posDialogWindow">
-                <canvas id="dialogWindow"></canvas>
+            <div class="text-course">
+                <p class="header-course">Название курса курса</p>
+                <p class="description-course">Описание курса описание курса описание курса</p>
+                <p class="time-course">2 часа</p>
             </div>
-            <p class="fs-5 posTextTrovald">Приветствую, меня зовут Торвальд, я буду твоим спутником на всем твоем сложном пути познания. <br><br>
 
-                Чтобы я смог делиться с тобой своими знаниями тебе необходимо авторизироваться!</p>
-            <div class="col posAuth">
-
-                <form class="auth" action="">
-                    <div class="input-group mb-4 p-3 sizeBlokAuth">
-                        <div class="col">
-                            <h3 class="mb-3">АВТОРИЗАЦИЯ</h3>
-                            <div>
-                                <input class=" col-sm-2 form-control mb-4 borderNone" type="text" placeholder="Логин">
-                                <input class=" col-sm-2 form-control mb-4 borderNone" type="password" placeholder="Пароль">
-                            </div>
-                            <div>
-                                <button class="btn btn-light  mt-4 borderNone">ВОЙТИ</button>
-                            </div>
-                        </div>
-                    </div>
-                </form>
+            <div class="img-course">
+                <img src="/public/img/icons/courses/books.svg">
             </div>
         </div>
-    </div>
-    <div class="container textDiv">
-        <p class="fs-2 colorOrange">Python</p>
-        <p class=" fs-5">Различные языки программирования обычно доминируют в какой-то отрасли (или нескольких), для работы в которой они хорошо подходят. Но это не значит, что программист ограничен использовать строго определённый инструмент, поэтому любой язык общего назначения, такой как Python, может применять для создания чего-угодно.</p>
-    </div>
-    <div class="colorBlock textDiv">
-        <p class="fs-2">Курсы</p>
+    </section>
+
+    <div class="watch-all-courses">
+        <a href="#">Посмотреть всё</a>
     </div>
 </main>
 
+<?php include_once $_SERVER["DOCUMENT_ROOT"] . "/views/components/footer.php"; ?>
 
 <script src="/public/js/dialogWindow.js"></script>
-<?php include_once $_SERVER["DOCUMENT_ROOT"] . "/views/components/footer.php"; ?>
