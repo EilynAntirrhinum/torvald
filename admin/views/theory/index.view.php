2f1ce631793e5c1f82cae0c287de7d1e8b1c11f0
feradeth <gankachan130903@gmail.com>
2022-06-12 23:39:42 +0500

    change editor + add input text for practice

--- admin/views/theory/index.view.php
+++ admin/views/theory/index.view.php
@@ -1,6 +1,8 @@
 <?php include_once $_SERVER['DOCUMENT_ROOT'].'/views/components/header.php'?>
 <main>
+    <form method="post" enctype="multipart/form-data">
     <section>
+
         <div class="section">
             <input type="text" class="input input-section" name="section_name" placeholder="Название раздела">
 <!--            <button class="edit">-->
@@ -13,9 +15,9 @@
 <!--                &#9998;-->
 <!--            </button>-->
         </div>
-        <iframe id="out_editor" src="/admin/views/theory/editor.php" width="100%">
+        <textarea id="mytextarea" name="content">
 
-        </iframe>
+        </textarea>
     </section>
     <article>
         <p>Раздел 1</p>
@@ -27,11 +29,11 @@
             <li>Тема 1</li>
         </ul>
     </article>
-    <form method="post" action="/admin/app/theory/upload.php" enctype="multipart/form-data">
+
         <section class="practice">
             <div class="file">
                 <img src="/public/img/icons/practice.svg" alt="file">
-                Практика 1
+                <input type="text" id="pr_name" class="input" name="pr_name" placeholder="Название практики" size="50">
             </div>
             <div class="btns">
                 <label class="add">
@@ -45,6 +47,20 @@
         </section>
         <button id="save" name="save"> СОХРАНИТЬ </button>
     </form>
+    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+    <script>
+        tinymce.init({
+            selector: '#mytextarea',
+            plugins: [
+                'a11ychecker','advlist','advcode','advtable','autolink','checklist','export',
+                'lists','link','image','charmap','preview','anchor','searchreplace','visualblocks',
+                'powerpaste','fullscreen','formatpainter','insertdatetime','media','table','help','wordcount'
+            ],
+            toolbar: 'undo redo | formatpainter casechange blocks | bold italic backcolor | ' +
+                'alignleft aligncenter alignright alignjustify | ' +
+                'bullist numlist checklist outdent indent | removeformat | a11ycheck code table help'
+        });
+    </script>
 </main>
 
 
