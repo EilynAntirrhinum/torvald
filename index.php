1f601a0b6f4d5598ad2c52a2e5dc10cc82e93e69
EilynAntirrhinum <elyrhinum@gmail.com>
2022-06-13 13:47:50 +0300

    before merge rhinum and main

--- index.php
+++ index.php
@@ -3,5 +3,11 @@
 $title = "Главная";
 $style = "main-page.css";
 
+//if(!$_SESSION["isAuth"]) {
+//    include $_SERVER["DOCUMENT_ROOT"] . "/index.view.php";
+//}
+//else if ($_SESSION["isAuth"]) {
+//    include $_SERVER["DOCUMENT_ROOT"] . "/views/authorized-main-page/index.view.php";
+//}
 
 include $_SERVER["DOCUMENT_ROOT"] . "/index.view.php";
